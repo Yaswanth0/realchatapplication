2fafcd3d95d0eeddc8923418ca36c11e46bf7f34
Yaswanth0 <yaswanthnallamalli@gmail.com>
2025-10-06 23:25:23 +0530

    feat: add ChatMessageRepository extends

--- src/main/java/com/example/realchatapplication/repository/ChatMessageRepository.java
+++ src/main/java/com/example/realchatapplication/repository/ChatMessageRepository.java
@@ -1,4 +1,7 @@
 package com.example.realchatapplication.repository;
 
-public interface ChatMessageRepository {
+import com.example.realchatapplication.model.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 }
